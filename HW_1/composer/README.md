13bea44eae14b99b3881ec689c847748663288cd
Yura <bobr515yura@yandex.ru>
2025-07-03 21:26:32 +0300

    HW

---
+++ HW_1/composer/README.md
@@ -0,0 +1,17 @@
+В качестве решения пришлите MD-файл с кодом/скриншотом созданного файла composer.json.
+
+{
+    "name": "hex/yura",
+    "description": "Домашнее задание по Composer",
+    "type": "library",
+    "license": "proprietary",
+    "authors": [
+        {
+            "name": "Yura"
+        }
+    ],
+    "minimum-stability": "dev",
+    "require-dev": {
+        "psr/log": "^3.0@dev"
+    }
+}
---
+++ HW_1/composer/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hex/yura',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hex/yura' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(
+                0 => '3.x-dev',
+            ),
+            'dev_requirement' => true,
+        ),
+    ),
+);
